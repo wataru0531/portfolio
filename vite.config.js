367173bb869651c70b99add78d707ca0588e713b
wataru0531 <obito0531@gmail.com>
2025-12-17 22:32:49 +0900

    ポートを3000に変更

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+
+import { defineConfig } from "vite";
+
+
+export default defineConfig({
+  server: {
+    port: 3000, // 3000番でサーバーを立てる
+    strictPort: true, // 3000 が使われていたら
+                      // 勝手に 3001, 3002 に逃げず、エラーで止まる
+  }
+})
+
